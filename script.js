e57c4c1461b94bb72dbb4aea6114b2c142c190dd
Ilya Fofanov <66513936+Nihaochingiz@users.noreply.github.com>
2021-05-12 17:25:58 +0300

    init
    add files for game

---
+++ script.js
@@ -0,0 +1,16 @@
+    
+    var burgers = 0;
+    function onClick() {
+        burgers += 1;
+        document.getElementById("burgerPlay").innerHTML = burgers;
+    };
+
+
+
+
+
+    var clicks = 0;
+    function onClick() {
+        clicks += 1;
+        document.getElementById("clicks").innerHTML = clicks;
+    };
